c7deed7d803ce509c04e295de9d215ecdd0fc046
edward <edwardlfish@gmail.com>
2025-08-31 21:14:02 +0300

    Use RTK Query transformResponse

--- src/features/api/weatherApi.ts
+++ src/features/api/weatherApi.ts
@@ -1,13 +1,25 @@
 import {api_key, base_url} from "../../utils/constants.ts";
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import type {WeatherInfoResponse} from "../../utils/types";
+import type {WeatherInfo, WeatherInfoResponse} from "../../utils/types";
 
+// TODO Homework 3 Propose ideas: How refetch weather info after user defined timeout
 export const weatherApi = createApi({
     reducerPath: 'weatherApi',
     baseQuery: fetchBaseQuery({baseUrl: base_url}),
+    // TODO Homework 2 learn refetchOnMountOrArgChange
+    // refetchOnMountOrArgChange:
     endpoints: builder => ({
-        getWeatherByCity: builder.query<WeatherInfoResponse,string>({
-            query: city => `?q=${city}&appid=${api_key}&units=metric`
+        getWeatherByCity: builder.query<WeatherInfo,string>({
+            query: city => `?q=${city}&appid=${api_key}&units=metric`,
+            // TODO Homework 1 learn keepUnusedDataFor
+            // keepUnusedDataFor:
+            transformResponse: (data: WeatherInfoResponse) => ({
+                city: data.name,
+                temp: data.main.temp,
+                pressure: data.main.pressure,
+                country: data.sys.country,
+                sunset: new Date(data.sys.sunset * 1000).toLocaleTimeString()
+            })
         })
     }),
 })
